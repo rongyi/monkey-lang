0e4c74186b4f3ca49dc2d0539539ed8c8f781efd
rongyi <sqrongyi@163.com>
2017-04-14 15:15:16 +0800

    Add return statement parser

--- ast/ast.go
+++ ast/ast.go
@@ -53,3 +53,15 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
+
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral return the ReturnStatement token literal
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -71,3 +71,30 @@ func dolettest(t *testing.T, s ast.Statement, name string) bool {
 	}
 	return true
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 8888;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	prog := p.ParseProgram()
+	checkParseError(t, p)
+
+	if len(prog.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(prog.Statements))
+	}
+	for _, stmt := range prog.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral not 'return', got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
