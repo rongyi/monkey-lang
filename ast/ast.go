0dfe7b9263aa8b9c013717f4d811c9968b5debb8
rongyi <sqrongyi@163.com>
2017-04-18 08:53:30 +0800

    Add bool ast

--- ast/ast.go
+++ ast/ast.go
@@ -182,3 +182,21 @@ func (es *ExpressionStatement) statementNode() {}
 func (es *ExpressionStatement) TokenLiteral() string {
 	return es.Token.Literal
 }
+
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {
+
+}
+
+
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
