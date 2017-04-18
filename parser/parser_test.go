d6c54e88bea4ce0f8ee5002670d47b9b8ace831a
rongyi <sqrongyi@163.com>
2017-04-18 13:21:32 +0800

    Update parser test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -625,7 +625,7 @@ func TestIfString(t *testing.T) {
 		t.Fatalf("program len not %d, got=%d", 2, len(program.Statements))
 	}
 
-	fmt.Println(program.String())
+	// fmt.Println(program.String())
 }
 
 func TestFunctionLiteralParsing(t *testing.T) {
@@ -673,4 +673,34 @@ func TestFunctionLiteralParsing(t *testing.T) {
 	}
 
 	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
+}
+
+func TestFunctionParameterParsing(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedParams []string
+	}{
+		{input: "fn() {};", expectedParams: []string{}},
+		{input: "fn(x) {};", expectedParams: []string{"x"}},
+		{input: "fn(x, y, z) {};", expectedParams: []string{"x", "y", "z"}},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParseError(t, p)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		if len(function.Parameters) != len(tt.expectedParams) {
+			t.Errorf("length parameters wrong. want %d, got=%d\n",
+				len(tt.expectedParams), len(function.Parameters))
+		}
+
+		for i, ident := range tt.expectedParams {
+			testLiteralExpression(t, function.Parameters[i], ident)
+		}
+	}
 }
