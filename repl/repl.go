3bc7b966b1c30d01cb8478fb76c5b22b0cbea4ac
rongyi <sqrongyi@163.com>
2017-04-19 09:14:51 +0800

    Parser repl done

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"io"
 	"monkey/lexer"
-	"monkey/token"
+	// "monkey/token"
+	"monkey/parser"
 )
 
 // PROMPT is the prompt of the shell
@@ -24,9 +25,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		prog := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParseError(out, p.Errors())
+			continue
 		}
+
+		// token repl
+		// for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+		// 	fmt.Printf("%+v\n", tok)
+		// }
+		io.WriteString(out, prog.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseError(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
