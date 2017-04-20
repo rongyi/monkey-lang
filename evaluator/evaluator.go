cd30963e49b41caac14637cd3d862ae2d441a089
rongyi <sqrongyi@163.com>
2017-04-20 17:46:20 +0800

    IfExpression done

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,9 +1,9 @@
 package evaluator
 
 import (
+	// "fmt"
 	"monkey/ast"
 	"monkey/object"
-	"fmt"
 )
 
 var (
@@ -15,6 +15,8 @@ var (
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.BlockStatement:
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
@@ -28,11 +30,13 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
 	case *ast.InfixExpression:
-		fmt.Println("infix calculation")
-		fmt.Println(node.String())
+		// fmt.Println("infix calculation")
+		// fmt.Println(node.String())
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
@@ -124,5 +128,28 @@ func evalIntegerInfixExpression(op string, left, right object.Object) object.Obj
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
